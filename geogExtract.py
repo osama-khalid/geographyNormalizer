fde2cc7e940954928e6cf953c635f4e3e85456eb
osama-khalid <khalyd.osama@gmail.com>
2019-03-12 15:39:34 -0500

    Add files via upload

--- geogExtract.py
+++ geogExtract.py
@@ -1,3 +1,12 @@
+country=open('countries.txt','r').read().split('\n')
+countries=[]
+
+for c in country:
+    if len(c)>0:
+        row=c.split('\t')
+        if len(row[1])>0:
+            countries.append(row[3].lower())
+
 text=open('xx.txt','r').read().split('\n')
 from nltk import ngrams
 for f in text:
@@ -7,12 +16,18 @@ for f in text:
             location=row[4]
             text=location.strip(' ').lower()
             print(text.split(' '))
+            if text.split(' ') [-1] in countries:
+                print('-------')
             bigrams = ngrams(text.split(), 2)
             b=[]
             for i in bigrams:
                 b.append(' '.join(list(i)))
+                if ' '.join(list(i)) in countries:
+                    print('-------')
             print(b)
             print(location.replace(' ','').lower())
+            if location.replace(' ','').lower() in countries:
+                print('-------')
             
             
 cities=open('cities.txt','r').read().split('\n')
